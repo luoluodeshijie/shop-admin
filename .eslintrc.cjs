451a3ba530a4ea29933e7d8665b2f5fb980c4cc9
李铭贤 <limingxian@limingxiandeMacBook-Pro.local>
2022-12-26 00:08:37 +0800

    修改 eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,18 +4,22 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
     'standard-with-typescript'
   ],
   overrides: [
   ],
+  parser: 'vue-eslint-parser', // 解析vue
   parserOptions: {
+    parser: '@typescript-eslint/parser', // 解析ts HelloWorld.vue 报错  4:29  error  Parsing error: Unexpected token )
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json'  // eslint规则 Error: Error while loading rule '@typescript-eslint/dot-notation': You have used a rule which requires parserServices to be generated. You must therefore provide a value for the "parserOptions.project" property for @typescript-eslint/parser.
   },
   plugins: [
     'vue'
   ],
   rules: {
+    "@typescript-eslint/triple-slash-reference": "off" // vite-env.d.ts 报错  1:1  error  Do not use a triple slash reference for vite/client, use `import` style instead  @typescript-eslint/triple-slash-reference
   }
 }

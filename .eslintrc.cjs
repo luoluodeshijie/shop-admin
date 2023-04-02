926e36effa1f2613f129f4e7290240bbac172cc9
limingxian <limingxianweb@163.com>
2023-04-02 22:18:58 +0800

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,17 +9,20 @@ module.exports = {
   ],
   overrides: [
   ],
-  parser: 'vue-eslint-parser', // 解析vue
+  // parser: 'vue-eslint-parser', // 解析vue
+  // parser: '@typescript-eslint/parser',
   parserOptions: {
     parser: '@typescript-eslint/parser', // 解析ts HelloWorld.vue 报错  4:29  error  Parsing error: Unexpected token )
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'  // eslint规则 Error: Error while loading rule '@typescript-eslint/dot-notation': You have used a rule which requires parserServices to be generated. You must therefore provide a value for the "parserOptions.project" property for @typescript-eslint/parser.
+    project: ['tsconfig.json'], // eslint规则 Error: Error while loading rule '@typescript-eslint/dot-notation': You have used a rule which requires parserServices to be generated. You must therefore provide a value for the "parserOptions.project" property for @typescript-eslint/parser.
+    extraFileExtensions: ['.vue'] // The extension for the file (.vue) is non-standard. You should add parserOptions.extraFileExtensions to your config。
   },
   plugins: [
-    'vue'
+    'vue',
+    '@typescript-eslint'
   ],
   rules: {
-    "@typescript-eslint/triple-slash-reference": "off" // vite-env.d.ts 报错  1:1  error  Do not use a triple slash reference for vite/client, use `import` style instead  @typescript-eslint/triple-slash-reference
+    '@typescript-eslint/triple-slash-reference': 'off' // vite-env.d.ts 报错  1:1  error  Do not use a triple slash reference for vite/client, use `import` style instead  @typescript-eslint/triple-slash-reference
   }
 }
